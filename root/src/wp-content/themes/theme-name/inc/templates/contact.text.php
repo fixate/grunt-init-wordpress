a5d478ea6dd7f28503ba80c8faf7d720f2dfebbb
Larry Botha <larry@fixate.it>
2013-07-04 15:24:30 +0200

    test merging subtree

--- root/src/wp-content/themes/theme-name/inc/templates/contact.text.php
+++
@@ -1,5 +0,0 @@
-Hi there,
-
-You have recieved an online request from <?php echo $mail['name'] ?>
-
-<?php echo $mail_message ?>
--- root/src/wp-content/themes/theme-name/inc/widgets.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-// Include and register widgets
-// include_once 'widgets/foo.php'
-// add_action('widgets_init', create_function('', 'register_widget( "foo_widget");'));

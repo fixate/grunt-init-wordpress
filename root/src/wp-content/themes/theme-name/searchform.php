117d61cf013833e158aa2c4dae8341b1aada32bb
Larry Botha <larry@fixate.it>
2013-07-05 16:05:58 +0200

    remove theme

--- root/src/wp-content/themes/theme-name/searchform.php
+++
@@ -1,3 +0,0 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/" role="search">
-	<input type="text" name="s" id="s"  placeholder="<?php _e('Search', 'theme_local'); ?>">
-</form>
